3b895e9c5ef0bdb4f7e822ccda73f374d029be8f
Louis Pelissier <ljpelissier@users.noreply.github.com>
2017-11-30 14:37:13 -0500

    Create newPiConfig.sh

---
+++ newPiConfig.sh
@@ -0,0 +1,20 @@
+#For new raspberry pi
+#install os first
+#Installation of the following
+# Appache2
+# PHP7
+# Mysql
+# SSH enabeled
+
+#COLORS
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+
+echo -e "$Red Hi there! $Green This is a great success! $Color_Off"
